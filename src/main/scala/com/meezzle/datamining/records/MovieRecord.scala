dc4c2300cba57ba837d4ca93ad33f0f7f8a2ad96
Stéphane Wong <stephane.wong1@gmail.com>
2019-03-05 23:24:14 +0100

    update movie record class

--- src/main/scala/com/meezzle/datamining/records/MovieRecord.scala
+++ src/main/scala/com/meezzle/datamining/records/MovieRecord.scala
@@ -5,11 +5,38 @@ case class BelongCollection(id: Int,
                             poster_path: String,
                             backdrop_path: String)
 
+case class ProductionCompany(name: Option[String],
+                             id: Option[Int],
+                             logo_path: Option[String],
+                             origin_country: Option[String])
+
+case class ProductionCountry(iso_3166_1: Option[String],
+                             name: Option[String])
+
 case class Genre(id: Option[Int],name: Option[String])
+
+case class SpokenLanguage(iso_639_1: Option[String], name: Option[String])
 
 case class MovieDetailRecord(adult: Option[Boolean],
                              backdrop_path: Option[String],
                              belongs_to_collection: Option[BelongCollection],
                              budget: Option[Int],
-                             genres: Option[Array[Genre]]
+                             genres: Option[Array[Genre]],
+                             homepage: Option[String],
+                             id: Option[Int],
+                             imdb_id: Option[String],
+                             original_language: Option[String],
+                             original_title: Option[String],
+                             overview: Option[String],
+                             popularity: Option[Double],
+                             poster_path: Option[String],
+                             production_companies: Option[Array[ProductionCompany]],
+                             production_countries: Option[Array[ProductionCountry]],
+                             spoken_languages: Option[Array[SpokenLanguage]],
+                             status: Option[String],
+                             tagline: Option[String],
+                             title: Option[String],
+                             video: Option[Boolean],
+                             vote_average: Option[Double],
+                             cote_count: Option[Int]
                             ) extends ApiRecord
