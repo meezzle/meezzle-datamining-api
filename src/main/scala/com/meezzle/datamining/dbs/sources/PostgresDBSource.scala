b766af1c62b50394766f27e0c2322c684975968c
Stéphane Wong <stephane.wong-ext@seloger.com>
2019-05-12 22:23:29 +0200

    add postgres source

---
+++ src/main/scala/com/meezzle/datamining/dbs/sources/PostgresDBSource.scala
@@ -0,0 +1,32 @@
+package com.meezzle.datamining.dbs.sources
+
+import com.typesafe.config.Config
+
+case class PostgresDBSource(config: Config,
+                            optSourceBuilder: Option[PostgresDBSourceBuilder] = None)
+  extends DBSource(config, optSourceBuilder) {
+  override protected def getSourceConfig: DBSourceConfig = {
+    optSourceBuilder.getOrElse(PostgresDBSourceBuilder()).build(config)
+  }
+}
+
+
+case class PostgresDBSourceBuilder() extends DBSourceBuilder {
+
+  import PostgresDBSourceBuilder._
+
+  override def build(config: Config): DBSourceConfig = {
+    val host = config.getString(s"$namespace.host")
+    val port = config.getInt(s"$namespace.port")
+    val db = config.getString(s"$namespace.db")
+    val user = config.getString(s"$namespace.username")
+    val pwd = config.getString(s"$namespace.password")
+
+    DBSourceConfig(host,port,db,user,pwd)
+  }
+}
+
+
+object PostgresDBSourceBuilder {
+  val namespace = s"${DBSourceBuilder.namespace}.postgres"
+}
